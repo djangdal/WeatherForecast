134230aa285e9bd3fb5f16f0006d81ee988d8a51
David Jangdal <djangdal@gmail.com>
2023-04-04 22:47:16 +0200

    Created empty project

---
+++ WeatherForecast/WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by David Jangdal on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
