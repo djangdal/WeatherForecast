1db3c65d8848cd88b386d41a6ac8e21c982bba56
David Jangdal <djangdal@gmail.com>
2023-04-05 12:24:31 +0200

    Improved async fetching of forecasts

--- WeatherForecast/WeatherForecast/ForecatListViewModel.swift
+++ WeatherForecast/WeatherForecast/ForecatListViewModel.swift
@@ -14,29 +14,66 @@ final class ForecastListViewModel: ForecastListViewModelProtocol {
 
     @Published var forecasts: [ForecastInfo] = []
 
-    private let forecastLocations: [ForecastLocation]
+    private let cities: [City]
 
     init(weatherFetcher: WeatherFetcher) {
         self.weatherFetcher = weatherFetcher
-        forecastLocations = [ForecastLocation(cityName: "Gothenburg",
-                                              coordinate: CLLocationCoordinate2D(latitude: 57.708870, longitude: 11.974560))]
+        cities = [City(name: "Gothenburg",
+                       coordinate: CLLocationCoordinate2D(latitude: 57.708870, longitude: 11.974560)),
+                  City(name: "Mountain View",
+                       coordinate: CLLocationCoordinate2D(latitude: 37.386051, longitude: -122.083855)),
+                  City(name: "London",
+                       coordinate: CLLocationCoordinate2D(latitude: 51.503399, longitude: -0.119519)),
+                  City(name: "New York",
+                       coordinate: CLLocationCoordinate2D(latitude: 40.712772, longitude: -74.006058)),
+                  City(name: "Berlin",
+                       coordinate: CLLocationCoordinate2D(latitude: 52.520008, longitude: 13.404954))]
     }
 
 
     @MainActor func fetchForecasts() async {
         do {
-            let req = forecastLocations.first!
-            let response = try await weatherFetcher.fetchWeather(for: forecastLocations.first!.coordinate)
-            print("Got response: \(response.properties)")
-            forecasts.append(ForecastInfo(cityName: req.cityName, symbolName: response.properties.timeseries.first!.data.next_1_hours?.summary.symbol_code ?? "asd"))
+            forecasts = try await cities.concurrentMap { [weak weatherFetcher] city in
+                let response = try await weatherFetcher?.fetchWeather(for: city.coordinate)
+                guard let symbolName = response?.properties.timeseries.first?.data.next_1_hours?.summary.symbol_code else {
+                    return ForecastInfo(cityName: city.name, symbolName: "cross")
+                }
+                return ForecastInfo(cityName: city.name, symbolName: symbolName)
+            }
         } catch {
-            print("Got error:  \(error)")
+            print("Could not refresh forecasts: \(error)")
         }
     }
 }
 
-struct ForecastLocation: Identifiable {
+struct City: Identifiable {
     let id = UUID()
-    let cityName: String
+    let name: String
     let coordinate: CLLocationCoordinate2D
+}
+
+private extension Sequence {
+    func asyncMap<T>(_ transform: (Element) async throws -> T) async rethrows -> [T] {
+        var values = [T]()
+
+        for element in self {
+            try await values.append(transform(element))
+        }
+
+        return values
+    }
+}
+
+private extension Sequence {
+    func concurrentMap<T>(_ transform: @escaping (Element) async throws -> T) async throws -> [T] {
+        let tasks = map { element in
+            Task {
+                try await transform(element)
+            }
+        }
+
+        return try await tasks.asyncMap { task in
+            try await task.value
+        }
+    }
 }
